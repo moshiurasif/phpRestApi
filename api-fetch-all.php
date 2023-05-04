810a8a4cc95281597982192ea3937996f1a0de95
asif2212 <asifmd2212@gamil.com>
2023-05-04 20:21:13 +0600

    first api create

--- api-fetch-all.php
+++ api-fetch-all.php
@@ -1,2 +1,15 @@
 <?php
+header("Content-type: application/json");
+header("Access-Control-Allow-Origin: *");
 include 'config.php';
+$sql = "SELECT * FROM students";
+$result = $conn->query($sql) or die("Query failed");
+$output = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $output[] = $row;
+    }
+    echo json_encode($output);
+} else {
+    echo json_encode(array("message" => "No Record Found", "status" => false));
+}
